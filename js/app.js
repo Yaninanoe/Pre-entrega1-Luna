877b9c82bb9f65d19caad729cd251d74f1fc40a2
Yaninanoe <yaninanoeluna@gmail.com>
2024-04-14 17:46:55 -0300

    Funciones carrito de compras

--- js/app.js
+++ js/app.js
@@ -11,80 +11,124 @@ carrito.addEventListener ("click",()=>{
 /*-----------------------------*/
 
 const carProduct = document.querySelector(".car-product")
-const carritoCompras = document.querySelector(".carrito-compras")
-
-
-/*Lista de los detalles de los productos*/ 
-
 const contadorProductosCarrito = document.querySelector('.contador-productos-carrito')
 
-//const padreFotos = document.querySelector (".padre-fotos")
+
+
+
+/*Lista de todos los productos*/ 
+
+
+const padreFotos = document.querySelector (".padre-fotos")
 
 /*Variables de arreglo para productos */
 
 let allProducto = []
 
+const totalPagar = document.querySelector('.total-pagar')
+const contadorPts = document.querySelector('#contador')
 
 
 
-
-
-const cellphone = document.querySelector(".padre-fotos")
-//const btn = document.querySelector ('.btn-car')
-
-
-cellphone.addEventListener ('click', e=> {
+// UTILIZAR ID EN LUGAR DE TÍTULOS REALIZAR EL CAMBIO EN LOS EVENTOS
+padreFotos.addEventListener ('click', e=> {
     if(e.target.classList.contains('btn-car')){
         const productoSec = e.target.parentElement
         const infoProductos = {
             quantity: 1,
             titulo: productoSec.querySelector('p').textContent,
             precio: productoSec.querySelector('h3').textContent,
+        };
+
+        const existProduct = allProducto.some(productoSec => productoSec.titulo === infoProductos.titulo)
+        
+if (existProduct){
+    const products = allProducto.map(productoSec=>{
+        if(productoSec.titulo === infoProductos.titulo){
+            productoSec.quantity++;
+            return productoSec
+        }else {
+            return productoSec
         }
-        allProducto = [...allProducto, infoProductos]
+    })
+    allProducto = [...products]
+} else {
+    allProducto = [...allProducto, infoProductos];
+}
+
+     
         //llamamos la funcion mostrarHTML
         mostrarHTML();
     }
-    
-
-    //document.querySelector (".celulares")
-    //console.log("producto")
 })
 
+//UTILIZAR ID PARA LOS EVENTOS CAMBIAR EN LUGAR DE TÍTULO
+
+contadorProductosCarrito.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('icon')){
+        const productoSec = e.target.parentElement
+        const titulo = productoSec.querySelector('p').textContent;
+        allProducto = allProducto.filter(
+            productoSec => productoSec.titulo !== titulo
+        );
+        console.log(allProducto)
+        mostrarHTML()
+    }
+})
 /*Funcion para mostrar en HTML*/
 
 const mostrarHTML = () => {
 
     contadorProductosCarrito.innerHTML = '';
 
-    allProducto.forEach(producto => {
+    let total = 0;
+    let totalcompra = 0;
+
+    allProducto.forEach(productoSec => {
         const contenedorProducto = document.createElement('div')
         contenedorProducto.classList.add('car-product')
         contenedorProducto.innerHTML =`
         <div class="info-car-product">
-                            <span> ${producto.quantity} </span>
-                            <p class="info-car-titulo">${producto.titulo}</p>
-                            <span>${producto.precio}</span>
+                            <span> ${productoSec.quantity} </span>
+                            <p class="info-car-titulo">${productoSec.titulo}</p>
+                            <span>${productoSec.precio}</span>
                         </div>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                           </svg>`
-                          contadorProductosCarrito.appened(contenedorProducto);
-    })
-    
+                          contadorProductosCarrito.append(contenedorProducto);
+
+        total = total + parseInt(productoSec.quantity * productoSec.precio.slice(1))
+        totalcompra = totalcompra + productoSec.quantity;
+    });
+
+    totalPagar.innerText = `$ ${total}`;
+    contadorPts.innerText = totalcompra;
+    console.log(totalPagar)
+    sincronizarStorageCar()
+};
+
+//LOCAL STORAGE
+
+function sincronizarStorageCar(){
+    localStorage.setItem('allProducto', JSON.stringify(allProducto))
 }
 
-//cellphone.addEventListener("click", e=>{
-//console.log()
-//})
+// DOM LOCAL STORAGE
 
+document.addEventListener('DOMContentLoaded', ()=>{
 
+allProducto = JSON.parse(localStorage.getItem('allProducto')) || []
+
+})
 
 
 //FUNCIONES PARA FORMULARIO
 const mensajeContacto = document.querySelector(".contacto")
 const listaComentario = document.querySelector("#lista-comentarios");
+
 let comentarios = [];
+
 
 function mostrarAlerta(msj){
 const mensajeAlerta = document.createElement('p')
@@ -103,7 +147,7 @@ mensajeAlerta.remove();
 
 function agregarComentario(e){
 e.preventDefault()
-//console.log("Agregaste comentario")
+
 const comentario = document.querySelector("#mensaje").value;
 
 comentario === '' && mostrarAlerta("Debe ingresar el mensaje")
@@ -128,15 +172,18 @@ comentarios.forEach(item =>{
     li.textContent = item.texto;
     li.setAttribute('id', item.id)
     listaComentario.appendChild(li)
-})
-function limpiarComentarios(){
-    while(listaComentario.firstChild){
-        listaComentario.removeChild(listaComentario.firstChild)
-    }
-}
-sincronizarStorage()
+})}
 
+
+
+function limpiarComentarios(){
+ while(listaComentario.firstChild){
+       listaComentario.removeChild(listaComentario.firstChild)
+  }
+  sincronizarStorage()
 }
+
+
 // FUNCION PARA QUE SE GUARDEN LOS COMENTARIOS EN EL STORAGE
 function sincronizarStorage(){
     localStorage.setItem('comentarios', JSON.stringify(comentarios))
@@ -145,14 +192,11 @@ function sincronizarStorage(){
 mensajeContacto.addEventListener("submit", agregarComentario);
 
 
+
 // DOM LOCAL STORAGE
 
 document.addEventListener('DOMContentLoaded', ()=>{
-/*if (JSON.parse(localStorage.getItem ("comentarios"))){
-    comentarios = JSON.parse(localStorage.getItem('comentarios'))
-}else {
-    comentarios = [];
-}*/
+
 comentarios = JSON.parse(localStorage.getItem('comentarios')) || []
 mostrarComentarioHTML()
 })
@@ -169,84 +213,6 @@ mostrarComentarioHTML()
 
 
 
-/*function mostrarAlerta (mensaje) {
-    alert(mensaje);
-}
-mostrarAlerta ("Bienvenido a CellphoneArgentina")*/
-
-
-//let marca = prompt ("Ingrese la Marca que desee buscar")
-//let electro = prompt ("Ingrese el producto")
-
-//if para ingreso de marca del producto
-
-/*if (!marca) {
-    mostrarAlerta ("Por favor, ingrese la marca del producto")
-    
-} else {
-    mostrarAlerta ("Ingresaste la marca " + marca)
-}
-
-const datosBusqueda = {
-    marca: marca.trim().toLowerCase()
-};
-// if para tipo de producto
-
-/*if (!electro) {
-    mostrarAlerta ("Por favor, ingrese el producto")
-    
-} else {
-    mostrarAlerta ("Ingresaste " + electro)
-}
-
-const datosBusqueda = {
-    electro: electro.trim().toLowerCase()
-};*/
-
-
-
-
-/*function verItems (modelo) {
-    modelo.forEach(item => {
-        console.log(item.marca + " - " + item.nombre + " - " + item.precio + " - " + item.color)
-    });
-    ;
-}
-
-verItems (items)
-
-function filtrarMarca (item){
-if (datosBusqueda.marca){
-    return item.marca.trim().toLowerCase() === datosBusqueda.marca;
-}
-return true
-}
-
-function filtrarItems () {
-    const resultado = items.filter(filtrarMarca);
-    if(resultado.length>0) {
-        mostrarItems(resultado)
-    }
-    else {
-        console.error("No se encontraron resultados")
-        mostrarAlerta("Lo sentimos, no se encontró la marca ingresada")
-    }
-}
-
-filtrarItems ()
-
-function mostrarItems (items) {
-    console.log ("Resultado de la búsqueda: ");
-    verItems(items);
-}
-
-function verElectro (productos) {
-    productos.forEach(producto => {
-        console.log(producto.marca + " - " + producto.nombre + " - " + producto.precio + " - " + producto.color)
-    });
-}
-
-verElectro(electro)*/
 
 
 
